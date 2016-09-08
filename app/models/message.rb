3e0a6422dd8a8e6bdab92d09c7f94847020e951c
Udit Kapahi <ukitkk@gmail.com>
2016-09-08 14:21:10 +0530

    added messages controller model and views

---
+++ app/models/message.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text(65535)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Message < ActiveRecord::Base
+end
